53f5b779fa2b9a13ecd61259cd5c724fbe9155b0
Siraj Qazi <siraj.qazi@seeloz.com>
2024-12-10 01:23:45 +0500

    Initial Commit

---
+++ EliteCoderiOS/EliteCoderiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  EliteCoderiOSApp.swift
+//  EliteCoderiOS
+//
+//  Created by blakberrisigma on 10/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct EliteCoderiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EliteCoderiOSTests/EliteCoderiOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  EliteCoderiOSTests.swift
+//  EliteCoderiOSTests
+//
+//  Created by blakberrisigma on 10/12/2024.
+//
+
+import Testing
+@testable import EliteCoderiOS
+
+struct EliteCoderiOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
